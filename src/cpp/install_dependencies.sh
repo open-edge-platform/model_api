0ec186decc28d9b7ad7fd3596e89e5dc2c95374f
Ronald Hecker <ronald.hecker@intel.com>
2025-04-10 20:59:33 +0200

    Add Dockerfile for building the python bindings from c++
    It's ugly, but its just an example to show it works

--- src/cpp/install_dependencies.sh
+++ src/cpp/install_dependencies.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+test -f /usr/share/doc/kitware-archive-keyring/copyright ||
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ jammy main' | tee /etc/apt/sources.list.d/kitware.list >/dev/null
+apt-get update
+apt-get install -y cmake
+
 # Added required keys / do the update
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 
 apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 
-echo "deb https://apt.repos.intel.com/openvino/2025 ubuntu22 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2025.list
+echo "deb https://apt.repos.intel.com/openvino ubuntu22 main" | tee /etc/apt/sources.list.d/intel-openvino.list
 
 apt update
 
 #Install OpenCV
-apt-get install libopencv-dev
+apt-get install -y libopencv-dev
 
 # Install OpenVINO
-sudo apt install openvino-2025.0.0
+apt install -y openvino-2025.1.0
