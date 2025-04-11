bc001ffbce6783a76341072e6f8a56366b128f8e
Ronald Hecker <ronald.hecker@intel.com>
2025-04-11 11:25:46 +0200

    Remove rpath setting

--- src/cpp/py_bindings/src/vision_api/__init__.py
+++ src/cpp/py_bindings/src/vision_api/__init__.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+try:
+    from openvino import Core
+    _ = Core()  # Triggers loading of shared libs like libopenvino.so
+except Exception as e:
+    raise ImportError(f"Failed to initialize OpenVINO runtime: {e}")
+
 from ._vision_api import Classification, ClassificationModel
 
 
