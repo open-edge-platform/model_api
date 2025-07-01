7def64e50ad2c35ea7ffeda2e6a6e1e89396e31a
Vladisalv Sovrasov <sovrasov.vlad@gmail.com>
2025-07-01 19:27:38 +0900

    Add a unit test

---
+++ tests/python/unit/test_types.py
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2025 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from model_api.models.types import ListValue
+
+
+def test_string_list_parameter():
+    str_list = ListValue(
+        value_type=str,
+        description="List of strings",
+        default_value=["label1", "label2", "label3"],
+    )
+    assert str_list.value_type == str
+
+    parsed_list = str_list.from_str("1 2 3")
+
+    assert len(parsed_list) == 3
+    assert type(parsed_list[0]) is str
