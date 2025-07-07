df479e712e45110e30076fa03ff13d1812af619f
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2025-07-07 16:25:40 +0200

    Add keypoint detection model (#327)
    * Enable parsing resize parameters from model config
    
    * Add a test for MRCNN resize_letterbox
    
    * Read resize from config in ssd
    
    * Fix build
    
    * Prevent anymap from trying to parse u8 type
    
    * Enum to enum class in ssd
    
    * Add keypoint detection
    
    * Make cpp keep_ratio preprocessing graph identical to python

--- src/cpp/include/tasks/results.h
+++ src/cpp/include/tasks/results.h
@@ -227,3 +227,27 @@ struct AnomalyResult {
         return ss.str();
     }
 };
+
+struct KeypointDetectionResult {
+    std::vector<cv::Point2f> keypoints;
+    std::vector<float> scores;
+
+    friend std::ostream& operator<<(std::ostream& os, const KeypointDetectionResult& prediction) {
+        float kp_x_sum = 0.f;
+        for (const cv::Point2f& keypoint : prediction.keypoints) {
+            kp_x_sum += keypoint.x;
+        }
+        float scores_sum = std::accumulate(prediction.scores.begin(), prediction.scores.end(), 0.f);
+
+        os << "keypoints: (" << prediction.keypoints.size() << ", 2), keypoints_x_sum: ";
+        os << std::fixed << std::setprecision(3) << kp_x_sum << ", scores: (" << prediction.scores.size() << ",) "
+           << std::fixed << std::setprecision(3) << scores_sum;
+        return os;
+    }
+
+    explicit operator std::string() {
+        std::stringstream ss;
+        ss << *this;
+        return ss.str();
+    }
+};
