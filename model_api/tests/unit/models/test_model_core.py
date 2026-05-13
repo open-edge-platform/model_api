0d8b484bdee4cc7d84c6e0ed25db368f6ab475be
Mariusz Gumowski <mariusz.gumowski@intel.com>
2026-05-13 11:50:07 +0200

    format

--- model_api/tests/unit/models/test_model_core.py
+++ model_api/tests/unit/models/test_model_core.py
@@ -1243,5 +1243,8 @@ class TestDetectionModelEmptyImageBlobName:
             original_init(self_model, *args, **kwargs)
             self_model.image_blob_name = ""
 
-        with patch.object(ImageModel, "__init__", patched_init), pytest.raises(WrapperError, match="only one image input"):
+        with (
+            patch.object(ImageModel, "__init__", patched_init),
+            pytest.raises(WrapperError, match="only one image input"),
+        ):
             DetectionModel(adapter, configuration={}, preload=False)
